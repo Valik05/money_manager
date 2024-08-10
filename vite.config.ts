006352818351c22a2d01e78d9a82da683add199c
Valentin <chinchik38@gmail.com>
2024-08-10 11:43:58 +0200

    fix server config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 8000,
+    strictPort: true,
+    hmr: {
+      clientPort: 8000,
+      port: 5173,
+    },
     watch: {
       usePolling: true
     }
